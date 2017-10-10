ee1a9a270f108999f39acf53d9c2f70f380f93a4
OlofNymanson <onymanson@gmail.com>
2017-10-10 16:24:32 +0200

    fix infosida

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -5,10 +5,10 @@ function getMovieTitle(movieTitle) {
 			"Accept" : "application/json"
 		}
 	}).done(function(data) {
-		title = data['Title'];
-		$('#tableHeader').html('Sökresultat för: ' + title);
+		movieTitle = data['Title'];		
+		$('#tableHeader').html('Sökresultat för: ' + movieTitle);
 	});
-
+	
 	getMoviePoster(movieTitle);
 }
 
@@ -23,35 +23,34 @@ function getMoviePoster(movieTitle) {
 				poster = data['Poster'];
 				title = data['Title'];
 				row = '<tr><th><a href="infoPage.html"><img src="' + poster
-						+ '" onclick="getAllInfo('+title+')"></img></th><td>' + title + '</td></tr></a>';
+						+ '"></img></th><td>' + title + '</td></tr></a>';
+
 				$('#movieTable').html(row);
 			});
 }
 
-function getAllInfo(movieTitle){
+function getAllInfo() {
 	$.ajax({
-		url : 'http://omdbapi.com/?t=' + movieTitle + '&apikey=bbba3eae',
+//		url : 'http://omdbapi.com/?t=' + movieTitle + '&apikey=bbba3eae',
+		url: 'http://omdbapi.com/?t=Batman&apikey=bbba3eae',
 		headers : {
 			"Accept" : "application/json"
 		}
 	}).done(function(data) {
-		title = movieTitle;
-		genre = data['Genre'];
-		year = data['Year'];
-		length = data['Runtime'];
-		rating = data['imdbRating'];
-		plot = data['Plot'];
-		poster = data['Poster'];
-		
-		console.log(genre);
-		
-		$('#moviePoster').html('src="' + poster + '"');
-		$('#movieTitle').html(title);
-		$('#movieGenre').html(genre);
-		$('#movieYear').html(year);
-		$('#movieLength').html(length);
-		$('#movieRating').html(rating);
-		$('#moviePlot').html(plot);
+		 title = data['Title'];
+		 genre = data['Genre'];
+		 year = data['Year'];
+		 length = data['Runtime'];
+		 rating = data['imdbRating'];
+		 plot = data['Plot'];
+		 poster = data['Poster'];
+				
+		 $('#moviePoster').html('<img src="' + poster + '"/>');
+		 $('#movieTitle').html(title);
+		 $('#movieGenre').html(genre);
+		 $('#movieYear').html(year);
+		 $('#movieLength').html(length);
+		 $('#movieRating').html(rating);
+		 $('#moviePlot').html(plot);
 	});
 }
-
