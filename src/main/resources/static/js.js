953669f1d7306a9d9146bd89df69ee2342a3b9a4
OlofNymanson <onymanson@gmail.com>
2017-10-10 15:11:20 +0200

    försök att lägga till info i infosidan

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -1,6 +1,6 @@
 function getMovieTitle(movieTitle) {
 	$.ajax({
-		url : 'http://www.omdbapi.com/?t=' + movieTitle + '&apikey=bbba3eae',
+		url : 'http://omdbapi.com/?t=' + movieTitle + '&apikey=bbba3eae',
 		headers : {
 			"Accept" : "application/json"
 		}
@@ -23,7 +23,35 @@ function getMoviePoster(movieTitle) {
 				poster = data['Poster'];
 				title = data['Title'];
 				row = '<tr><th><a href="infoPage.html"><img src="' + poster
-						+ '"></img></th><td>' + title + '</td></tr></a>';
+						+ '" onclick="getAllInfo('+title+')"></img></th><td>' + title + '</td></tr></a>';
 				$('#movieTable').html(row);
 			});
 }
+
+function getAllInfo(movieTitle){
+	$.ajax({
+		url : 'http://omdbapi.com/?t=' + movieTitle + '&apikey=bbba3eae',
+		headers : {
+			"Accept" : "application/json"
+		}
+	}).done(function(data) {
+		title = movieTitle;
+		genre = data['Genre'];
+		year = data['Year'];
+		length = data['Runtime'];
+		rating = data['imdbRating'];
+		plot = data['Plot'];
+		poster = data['Poster'];
+		
+		console.log(genre);
+		
+		$('#moviePoster').html('src="' + poster + '"');
+		$('#movieTitle').html(title);
+		$('#movieGenre').html(genre);
+		$('#movieYear').html(year);
+		$('#movieLength').html(length);
+		$('#movieRating').html(rating);
+		$('#moviePlot').html(plot);
+	});
+}
+
